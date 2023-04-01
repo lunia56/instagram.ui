9bb4ec93ebc3329cc811113c9283526407d464a5
Ilya Lunin <lunia56@mail.ru>
2023-04-01 14:24:38 +0300

    initial commit

---
+++ src/pages/signup.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {NextPage} from 'next';
+
+const SignUpPage:NextPage = () => {
+    return (
+        <div>
+            <p>Hello world</p>
+        </div>
+    );
+};
+
+export default SignUpPage;
