9ce02516841ba67a69cb5f91c5a37752f07a9226
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2023-04-04 18:13:52 +0300

    fix: Headers

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './Header.module.scss'
 import logout from '../../assets/logout/logout.svg'
 import Image from 'next/image';
+import ModalLogout from '@/components/Modal/ModalLogOut/ModalLogout';
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+  console.log(show)
+  const openModalLogout = () =>{
+    setShow(true)
+  }
     return (
         <header className={s.header}>
           <div className={s.container}>
             <p className={s.title}>Instagram</p>
-            <div className={s.logout}>
+            <div className={s.logout} onClick={openModalLogout}>
               <Image src={logout} wigth={24} height={24} alt=""/>
               <p>Log Out</p>
             </div>
+            {show && <ModalLogout modalOnClick={()=> setShow(false)} />}
           </div>
         </header>
     );
