fc4753ad8c1a6060f844800173fa1e7d3cd46785
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2023-04-04 18:15:07 +0300

    feat: add modal

---
+++ src/pages/modal.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '@/components/Layout/Layout';
+import Modal from '@/components/Modal/Modal';
+
+const ModalPage = () => {
+    return (
+        <Layout>
+            <Modal/>
+        </Layout>
+
+    );
+};
+
+export default ModalPage;
