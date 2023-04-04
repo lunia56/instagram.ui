a35c7f9578e1413cc9a4a3aa880cf886c6213ed5
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2023-04-04 14:09:17 +0300

    fix: style Headers

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import s from './Header.module.scss'
+import logout from '../../assets/logout/logout.svg'
+import Image from 'next/image';
 
 const Header = () => {
     return (
-        <header>
-            <div className={s.header} ><span>Instagram</span></div>
+        <header className={s.header}>
+          <div className={s.container}>
+            <p className={s.title}>Instagram</p>
+            <div className={s.logout}>
+              <Image src={logout} wigth={24} height={24} alt=""/>
+              <p>Log Out</p>
+            </div>
+          </div>
         </header>
     );
 };
