a061747751d923588ca08c8255d2041a884d23e0
Andrey Schavelev <devandreyit@gmail.com>
2023-04-06 18:53:08 +0500

    Layout password recovery

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.scss'
 import Layout from '@/components/Layout/Layout';
+import PasswordRecovery from '@/components/PasswordRecovery/PasswordRecovery';
+import SignUp from '@/components/SignUp/SignUp';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +12,8 @@ export default function Home() {
   return (
       <Layout>
         <main className={styles.main}>
-          Content
+          <PasswordRecovery/>
+            {/*<SignUp/>*/}
         </main>
       </Layout>
   )
